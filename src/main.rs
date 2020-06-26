26d38392cd3ae533b183f0a576ac8e1e71d0d856
jclausius <jclausius@gmail.com>
2020-06-26 16:44:02 -0500

    var scope

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    {
+        // var s is not valid here (not yet declared)
+        let s = "hello";
+
+        // do stuff with s
+        let x = s.len();
+
+    } // scope is over... cannot use s as it is no longer valid
+
+    // if uncommented, error regarding use of s
+    //   let y = s.len();
+    
 }
